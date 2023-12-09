6e40671202aef86ac5a45a31190a0fca93d22db4
alexander231714 <alexander.carrillos@catolica.edu.sv>
2023-12-09 16:24:57 -0600

    Ver todos los productos en HomePage

--- src/componentes/HomePage.js
+++ src/componentes/HomePage.js
@@ -1,7 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
 const HomePage = () => {
+    const [productos, setProductos] = useState([]);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        fetchProductos();
+    }, []);
+
+    const fetchProductos = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/productos');
+            setProductos(response.data);
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    const handleProductClick = (producto) => {
+        setSelectedProduct(producto);
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setSelectedProduct(null);
+    };
+
     return (
-        <h1>Hola bienvenido</h1>
-    )
-  };
-  
-  export default HomePage;
+        <div className="container">
+            <h1>Lista de Productos</h1>
+            <div className="row">
+                {productos.map((producto) => (
+                    <div key={producto.idproducto} className="col-md-4">
+                        <div className="card mb-4" onClick={() => handleProductClick(producto)}>
+                            <img
+                                src={producto.imagen}
+                                className="card-img-top"
+                                alt="Imagen del producto"
+                                style={{ width: '100%', height: '200px', objectFit: 'contain' }}
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">{producto.nombre_producto}</h5>
+                                <p className="card-text">Precio: ${producto.precio}</p>
+                                <p className="card-text">Existencias: {producto.stock}</p>
+                                <button className="btn btn-primary">Agregar al Carrito</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            {/* Modal para mostrar información detallada del producto */}
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{selectedProduct && selectedProduct.nombre_producto}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {selectedProduct && (
+                        <div>
+                            <div className="text-center mb-3">
+                                <img src={selectedProduct.imagen} alt="Imagen del producto" className="img-fluid" />
+                            </div>
+                            <div>
+                                <p className="mb-2">
+                                    <strong>Precio:</strong> ${selectedProduct.precio}
+                                </p>
+                                <p className="mb-2">
+                                    <strong>Existencias:</strong> {selectedProduct.stock}
+                                </p>
+                                <p className="mb-2">
+                                    <strong>Categoría:</strong> {selectedProduct.categoria ? selectedProduct.categoria.nombrecategoria : 'N/A'}
+                                </p>
+                                <p className="mb-2">
+                                    <strong>Proveedor:</strong> {selectedProduct.proveedor ? selectedProduct.proveedor.nombre_proveedor : 'N/A'}
+                                </p>
+                                <p className="mb-2">
+                                    <strong>Descripción:</strong> {selectedProduct.descripcion || 'N/A'}
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Cerrar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+};
+export default HomePage;
--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -2,9 +2,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-
-
+      <div class="container-fluid">
         <Link className="navbar-brand" to="HomePage">Inicio</Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav">
@@ -19,7 +17,6 @@ function Navbar() {
             </li>
           </ul>
         </div>
-
       </div>
     </nav>
   );
